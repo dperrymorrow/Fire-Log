00498d9d4635e4b7e76d5d1982dd0f0e2f48acf6
tudo75 <tudo75@hotmail.com>
2012-06-03 18:56:31 +0200

    Ialian language file
    Signed-off-by: tudo75 <tudo75@hotmail.com>

---
+++ language/italian/fire_log_lang.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Fire_log Sparks
+ *
+ * This file contains all italian language strings used by the Fire_log spark.
+ *
+ * @author          Nicola Tudino
+ * @copyright       Copyright (c) 2012
+ * @link            http://www.tudo75.com/
+ * @filesource      fire_log_lang.php
+ */
+// ---------------------------------------------------------------------------
+$lang[ 'fire_log_home' ] = 'Torna ad oggi';
+$lang[ 'fire_log_view' ] = 'Vedi il file';
+$lang[ 'fire_log_delete' ] = 'Elimina il file';
+$lang[ 'fire_log_today' ] = 'Oggi';
+$lang[ 'fire_log_not_found' ] = 'Mi dispiace, non trovo %log_file%.';
+$lang[ 'fire_log_file_deleted' ] = 'Fire Log rimosso con successo ';
+$lang[ 'fire_log_show_debug' ] = 'Mostra messuggi di tipo Debug';
+$lang[ 'fire_log_show_info' ] = 'Mostra messuggi di tipo Info';
+$lang[ 'fire_log_show_error' ] = 'Mostra messuggi di tipo Error';
+$lang[ 'fire_log_show_all' ] = 'Mostra tutto';
+$lang[ 'fire_log_no_results_found' ] = 'Non ci sono messaggi che soddisfano i tuoi criteri di ricerca.';
+$lang[ 'fire_log_delete_sure' ] = 'Vuoi veramente eliminare il file?';
+// new strings from the view file
+$lang[ 'fire_log_delete_current' ] = 'ELIMINA IL FILE CORRENTE';//delete current file
+$lang[ 'fire_log_delete_all' ] = 'ELIMINA TUTTI I FILE';//delete all files
+$lang[ 'fire_log_show_all' ] = 'MOSTRA TUTTI';//show all
+$lang[ 'fire_log_show_errors' ] = 'ERRORI';//errors
+$lang[ 'fire_log_show_info' ] = 'INFO';//info
+$lang[ 'fire_log_show_debug' ] = 'DEBUG';//debug
